e4b43496f89f4a6230a2c8f72af57a6c760160c3
Tony Coop <tony@thewholeworldwindow.co.uk>
2018-02-27 11:44:22 +0000

    Fixed connectedReduxRedirect

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,27 @@
-import { routerActions } from 'react-router-redux'
 import { connectedReduxRedirect } from 'redux-auth-wrapper/history4/redirect'
+import locationHelperBuilder from 'redux-auth-wrapper/history4/locationHelper'
+import { routerActions } from 'react-router-redux'
 import App from './App'
 import Home from 'containers/Home/Home'
 import Login from 'containers/Login/Loadable'
 import Register from 'containers/Register/Loadable'
 import Dashboard from 'containers/Dashboard/Loadable'
 
+const locationHelper = locationHelperBuilder({})
+
 const isAuthenticated = connectedReduxRedirect({
   redirectPath: '/login',
   authenticatedSelector: state => state.auth.user !== null,
-  redirectAction: routerActions.replace,
-  wrapperDisplayName: 'UserIsAuthenticated'
+  wrapperDisplayName: 'UserIsAuthenticated',
+  redirectAction: routerActions.replace
 })
 
 const isNotAuthenticated = connectedReduxRedirect({
-  redirectPath: '/',
+  redirectPath: (state, ownProps) => locationHelper.getRedirectQueryParam(ownProps) || '/',
   authenticatedSelector: state => state.auth.user === null,
-  redirectAction: routerActions.replace,
-  wrapperDisplayName: 'UserIsAuthenticated',
-  allowRedirectBack: false
+  allowRedirectBack: false,
+  wrapperDisplayName: 'UserIsNotAuthenticated',
+  redirectAction: routerActions.replace
 })
 
 export default [
