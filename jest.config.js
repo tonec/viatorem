366eed0f2948e87e0a2bbdc23abe5630de3fd523
Tony Coop <tony@thewholeworldwindow.co.uk>
2018-02-28 23:59:26 +0000

    Enables code coverage reporting.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   setupFiles: ['./jest.setup.js'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  modulePaths: ['src']
+  modulePaths: ['src'],
+  collectCoverage: true,
+  coverageFormats: ['json', 'html']
 }
