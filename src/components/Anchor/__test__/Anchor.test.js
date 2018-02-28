33aa691b84995a89ee6bfed9825170edc3918d10
Tony Coop <tony@thewholeworldwindow.co.uk>
2018-02-28 22:56:30 +0000

    Adds more tests for Anchor.

--- src/components/Anchor/__test__/Anchor.test.js
+++ src/components/Anchor/__test__/Anchor.test.js
@@ -2,21 +2,61 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Anchor from '../Anchor'
 
-describe('Anchor (snapshot)', () => {
+const linkText = 'Link text'
 
+describe('Anchor (snapshot)', () => {
   const props = {
     href: 'http://www.example.com',
-    onClick: () => {},
     className: 'test-class-name'
   }
 
   it('renders according to snapshot when enabled', () => {
-    const component = shallow(<Anchor {...props}>Link text</Anchor>)
+    const component = shallow(<Anchor {...props}>{linkText}</Anchor>)
     expect(component).toMatchSnapshot()
   })
 
   it('renders according to snapshot when disabled', () => {
-    const component = shallow(<Anchor {...props} disabled >Link text</Anchor>)
+    const component = shallow(<Anchor {...props} disabled >{linkText}</Anchor>)
     expect(component).toMatchSnapshot()
+  })
+})
+
+describe('Anchor (rendering)', () => {
+  const props = {
+    className: 'test-class-name'
+  }
+
+  it('renders the correct link text and classes', () => {
+    const component = shallow(<Anchor {...props}>{linkText}</Anchor>)
+    expect(component.find('a').text()).toEqual(linkText)
+    expect(component.find('a').hasClass(props.className)).toBe(true)
+  })
+})
+
+describe('Anchor (behavior)', () => {
+  let props
+
+  beforeEach(() => {
+    props = { onClick: jest.fn() }
+  })
+
+  it('required onClick prop', () => {
+    const component = shallow(<Anchor {...props} >{linkText}</Anchor>)
+    expect(component.props().onClick).toBeDefined()
+  })
+
+  it('handler should be called when clicked and is active', () => {
+    const component = shallow(<Anchor {...props} >{linkText}</Anchor>)
+    component.find('a').simulate('click', { preventDefault: () => {} })
+    expect(props.onClick).toHaveBeenCalled()
+  })
+
+  it('handler should not be called if clicked and disabled', () => {
+    const component = shallow(<Anchor {...props} disabled >{linkText}</Anchor>)
+    component.find('a').simulate('click', {
+      preventDefault: () => {},
+      stopPropagation: () => {}
+    })
+    expect(props.onClick).not.toHaveBeenCalled()
   })
 })
