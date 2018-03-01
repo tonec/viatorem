4443a8c1feef10d0a1092afba4120791bad5f15c
Tony Coop <tony@thewholeworldwindow.co.uk>
2018-03-01 20:42:08 +0000

    Removes config which was causing an error.

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   snapshotSerializers: ['enzyme-to-json/serializer'],
   modulePaths: ['src'],
   collectCoverage: false,
-  coverageFormats: ['json', 'html'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js'
