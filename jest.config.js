9d7c24ce05d3b909f1533d922e175212dc7fde8d
Tony Coop <tony@thewholeworldwindow.co.uk>
2018-03-01 20:34:47 +0000

    Disables coverage reporting.

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   setupFiles: ['./jest.setup.js'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   modulePaths: ['src'],
-  collectCoverage: true,
+  collectCoverage: false,
   coverageFormats: ['json', 'html'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
