17df59ef394b40d3f8724f53bb56725394ee8dd4
Tony Coop <tony@thewholeworldwindow.co.uk>
2018-03-02 12:40:40 +0000

    Adds coverage reporting config.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   snapshotSerializers: ['enzyme-to-json/serializer'],
   modulePaths: ['src'],
   collectCoverage: false,
+  collectCoverageFrom: [
+    'src/**/*.js'
+  ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js'
