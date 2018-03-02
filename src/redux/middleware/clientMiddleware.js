0f1747196e1fa7bfb383564ee6967ea40d6ce73a
Tony Coop <tony@thewholeworldwindow.co.uk>
2018-03-02 12:04:28 +0000

    Adds comments to clientMiddleware.

--- src/redux/middleware/clientMiddleware.js
+++ src/redux/middleware/clientMiddleware.js
@@ -1,18 +1,28 @@
 export default function clientMiddleware (helpers) {
   return ({ dispatch, getState }) => next => action => {
+
+    // Is thunk
     if (typeof action === 'function') {
       return action(dispatch, getState)
     }
 
     const { promise, types, ...rest } = action
+
+    // Is pain object
     if (!promise) {
       return next(action)
     }
 
+    // Is async
     const [REQUEST, SUCCESS, FAILURE] = types
+
+    // async action started
     next({ ...rest, type: REQUEST })
 
+    // Inject helpers (eg. apiClient as client) and dispatch to action promise
     const actionPromise = promise(helpers, dispatch)
+
+    // Dispatch appropriate SUCCESS or FAIL action depending on whether action promise is resolved/value is returned or rejected
     actionPromise
       .then(result => next({ ...rest, result, type: SUCCESS }), error => next({ ...rest, error, type: FAILURE }))
       .catch(error => {
